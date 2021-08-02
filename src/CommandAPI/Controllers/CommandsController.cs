54f85b04a90c7d54a2f2aa4acb4eff02c7da54f6
Mehrdad <mhrddmoradii@gmail.com>
2021-08-02 17:57:23 -0400

    Added Model and Mock Repository

--- src/CommandAPI/Controllers/CommandsController.cs
+++ src/CommandAPI/Controllers/CommandsController.cs
@@ -1,14 +1,32 @@
 using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
+using CommandAPI.Data;
+using CommandAPI.Models;
 
 namespace CommandAPI.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
     public class CommandsController : ControllerBase {
+        private readonly ICommandAPIRepo _repository;
+        public CommandsController(ICommandAPIRepo repo)
+        {
+            _repository = repo;
+        }
+
         [HttpGet]
-        public ActionResult<IEnumerable<string>> Get() {
-            return new string[] {"this", "is", "hard", "coded"};
+        public ActionResult<IEnumerable<Command>> GetAllCommands() {
+            var commandItems = _repository.GetAllCommands();
+            return Ok(commandItems);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Command> GetCommandsById(int id) {
+            var commandItem = _repository.GetCommandById(id);
+            if (commandItem == null) {
+                return NotFound();
+            }
+            return Ok(commandItem);
         }
     }       
 }
--- src/CommandAPI/Startup.cs
+++ src/CommandAPI/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using CommandAPI.Data;
 
 namespace CommandAPI
 {
@@ -16,6 +17,7 @@ namespace CommandAPI
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddScoped<ICommandAPIRepo, MockCommandAPIRepo>();
             services.AddControllers();
         }
 
